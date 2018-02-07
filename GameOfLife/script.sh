f878b6e35bf4bc36ce0ed2cd0a430467498d1a87
Mashiur Rahman <mashiur@uab.edu>
2018-02-07 09:12:55 -0600

    back up

---
+++ GameOfLife/script.sh
@@ -0,0 +1,5 @@
+./run  1000 1000 1000 | tee dmc_result/1000_1000_1000.txt
+./run  5000 5000 1000 | tee dmc_result/5000_5000_1000.txt
+./run  5000 5000 5000 | tee dmc_result/5000_5000_5000.txt
+./run  10000 10000 1000 | tee dmc_result/10000_10000_1000.txt
+./run  10000 10000 5000 | tee dmc_result/10000_10000_5000.txt
