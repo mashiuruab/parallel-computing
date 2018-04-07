2298d455dc86294ff8669141dd28ccf7e4acbd6f
Mashiur Rahman <mashiur@uab.edu>
2018-04-07 07:12:08 -0500

    back up

--- HomeWork-5/main.cpp
+++ HomeWork-5/main.cpp
@@ -21,7 +21,8 @@ void linear() {
         MPI_Status status;
         MPI_Recv(&recv_buf, 1, MPI_INT, MASTER_RANK, DEFAULT_CHANNEL, MPI_COMM_WORLD, &status);
 
-        cout<< "Rank : " << my_rank << ", Received : " << recv_buf << endl;
+        cout<< "Rank : " << my_rank << ", Received : "
+            << recv_buf << ", from rank : " << status.MPI_SOURCE << endl;
     }
 }
 
@@ -37,14 +38,16 @@ void ring() {
         MPI_Status status;
         MPI_Recv(&recv_buf, 1, MPI_INT, recv_from, DEFAULT_CHANNEL, MPI_COMM_WORLD, &status);
 
-        cout<< "Rank : "<<my_rank << ", received : " << recv_buf << endl;
+        cout<< "Rank : "<<my_rank << ", received : "
+            << recv_buf << ", from rank : " << status.MPI_SOURCE << endl;
     } else {
         int recv_from = my_rank - 1;
         int recv_buf;
         MPI_Status status;
         MPI_Recv(&recv_buf, 1, MPI_INT, recv_from, DEFAULT_CHANNEL, MPI_COMM_WORLD, &status);
 
-        cout<< "Rank : "<<my_rank << ", received : " << recv_buf << endl;
+        cout<< "Rank : "<<my_rank << ", received : "
+            << recv_buf << ", from rank : " << status.MPI_SOURCE << endl;
 
         int process_to_send = my_rank + 1;
         MPI_Send(&recv_buf, 1, MPI_INT, process_to_send, DEFAULT_CHANNEL, MPI_COMM_WORLD);
@@ -172,8 +175,8 @@ int recv_matrix[20] = {-1};
 
 void init_tree_matrix(int low_rank, int upper_rank) { // 0 & number_of_process - 1
     if ((low_rank >= upper_rank  - 1) && recv_matrix[upper_rank] != -1) {
-        cout<< "Returned for " << low_rank << " " << upper_rank << endl;
-        cout<< recv_matrix[upper_rank] << endl;
+        /*cout<< "Returned for " << low_rank << " " << upper_rank << endl;
+        cout<< recv_matrix[upper_rank] << endl;*/
         return;
     }
 
@@ -207,7 +210,7 @@ void tree(int low_rank, int upper_rank) {
     init_dummy_matrix();
     init_tree_matrix(low_rank, upper_rank);
 
-    cout<< "Send Matrix " << endl;
+    /*cout<< "Send Matrix " << endl;
     for(int c1=0;c1<number_of_process;c1++){
         cout<< c1 << " -> " ;
         for(int c2=0;send_matrix[c1][c2]!=-1;c2++) {
@@ -220,9 +223,34 @@ void tree(int low_rank, int upper_rank) {
     for(int c=0;c<number_of_process;c++){
         cout<< c << " -> " <<recv_matrix[c]<<endl;
     }
+
+    cout<< "Received  Result >>>>>>>>>>>>>>>" << endl;*/
+
+    if(my_rank ==  MASTER_RANK) {
+        int send_buf = 10;
+        for(int c=0;send_matrix[my_rank][c]!=-1;c++){
+            MPI_Send(&send_buf, 1, MPI_INT, send_matrix[my_rank][c], DEFAULT_CHANNEL, MPI_COMM_WORLD);
+        }
+    } else if(recv_matrix[my_rank] != -1) {// everyone  should  receive except master
+        int recv_buf;
+        MPI_Status status;
+        MPI_Recv(&recv_buf, 1, MPI_INT, recv_matrix[my_rank], DEFAULT_CHANNEL, MPI_COMM_WORLD, &status);
+
+        cout<< "Rank : " << my_rank << ", Received : "
+            << recv_buf << ", from rank : " << status.MPI_SOURCE << endl;
+
+        for(int c=0;send_matrix[my_rank][c]!=-1;c++){
+            MPI_Send(&recv_buf, 1, MPI_INT, send_matrix[my_rank][c], DEFAULT_CHANNEL, MPI_COMM_WORLD);
+        }
+    }
 }
 
 int main(int argc, char** argv) {
+
+    if(argc != 2) {
+        cout<<" arg1 = which algorithm to run " << endl;
+        exit(EXIT_FAILURE);
+    }
 
     MPI_Init(NULL, NULL);
 
@@ -231,18 +259,30 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
     if(number_of_process == 1) {
+        cout<<"only one process nothing to broadcast"<<endl;
         MPI_Finalize();
         return 0;
     }
 
-    //linear();
+    int c = atoi(argv[1]);
 
-    //ring();
-
-    //double_ring();
-
-    if(my_rank == MASTER_RANK) {
-        tree(0, number_of_process - 1);
+    switch (c) {
+        case 1:
+            if (my_rank == MASTER_RANK) cout<< "Linear BroadCast>>>>>" << endl;
+            linear();
+            break;
+        case 2:
+            if (my_rank == MASTER_RANK) cout<< "Ring BroadCast>>>>>" << endl;
+            ring();
+            break;
+        case 3:
+            if (my_rank == MASTER_RANK) cout<< "Double Ring BroadCast>>>>>" << endl;
+            double_ring();
+            break;
+        case 4:
+            if (my_rank == MASTER_RANK) cout<< "Tree BroadCast>>>>>" << endl;
+            tree(0, number_of_process - 1);
+            break;
     }
 
     // Finalize the MPI environment.
