5fb53997a40895ef9ef2de586d8549c9cf6832c9
Mashiur Rahman <mashiur@uab.edu>
2018-04-15 15:52:37 -0500

    back up

---
+++ HomeWork-5/non_blocking/double_ring_16.sh
@@ -0,0 +1,21 @@
+module load openmpi
+
+algo=3
+np=16
+
+mpirun -n $np ./run $algo 32 >> result/double_ring/$np.result
+mpirun -n $np ./run $algo 64 >> result/double_ring/$np.result
+mpirun -n $np ./run $algo 128 >> result/double_ring/$np.result
+mpirun -n $np ./run $algo 256 >> result/double_ring/$np.result
+mpirun -n $np ./run $algo 512 >> result/double_ring/$np.result
+mpirun -n $np ./run $algo 1024 >> result/double_ring/$np.result
+mpirun -n $np ./run $algo 2048 >> result/double_ring/$np.result
+mpirun -n $np ./run $algo 4096 >> result/double_ring/$np.result
+mpirun -n $np ./run $algo 8192 >> result/double_ring/$np.result
+mpirun -n $np ./run $algo 16384 >> result/double_ring/$np.result
+mpirun -n $np ./run $algo 32768 >> result/double_ring/$np.result
+mpirun -n $np ./run $algo 65536 >> result/double_ring/$np.result
+mpirun -n $np ./run $algo 131072 >> result/double_ring/$np.result
+mpirun -n $np ./run $algo 262144 >> result/double_ring/$np.result
+mpirun -n $np ./run $algo 524288 >> result/double_ring/$np.result
+mpirun -n $np ./run $algo 1048576 >> result/double_ring/$np.result
---
+++ HomeWork-5/non_blocking/tree_16.sh
@@ -0,0 +1,21 @@
+module load openmpi
+
+algo=4
+np=16
+
+mpirun -n $np ./run $algo 32 >> result/tree/$np.result
+mpirun -n $np ./run $algo 64 >> result/tree/$np.result
+mpirun -n $np ./run $algo 128 >> result/tree/$np.result
+mpirun -n $np ./run $algo 256 >> result/tree/$np.result
+mpirun -n $np ./run $algo 512 >> result/tree/$np.result
+mpirun -n $np ./run $algo 1024 >> result/tree/$np.result
+mpirun -n $np ./run $algo 2048 >> result/tree/$np.result
+mpirun -n $np ./run $algo 4096 >> result/tree/$np.result
+mpirun -n $np ./run $algo 8192 >> result/tree/$np.result
+mpirun -n $np ./run $algo 16384 >> result/tree/$np.result
+mpirun -n $np ./run $algo 32768 >> result/tree/$np.result
+mpirun -n $np ./run $algo 65536 >> result/tree/$np.result
+mpirun -n $np ./run $algo 131072 >> result/tree/$np.result
+mpirun -n $np ./run $algo 262144 >> result/tree/$np.result
+mpirun -n $np ./run $algo 524288 >> result/tree/$np.result
+mpirun -n $np ./run $algo 1048576 >> result/tree/$np.result
